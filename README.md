20f92fa76115b18a6de0375dea7bb1adb77a7a7f
Jasper De gooijer <jasperdegooijer@jaspers-mbp.localdomain>
2019-07-24 18:23:19 +0200

    finished fungible tokens up to allowances

---
+++ README.md
@@ -0,0 +1,2 @@
+# NEAR protocol ERC20-like token
+For reference checkout: [near-bindgen](https://github.com/nearprotocol/near-bindgen)
---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+#![feature(const_vec_new)]
+
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[macro_use]
+mod unlimited_allowance_token;
